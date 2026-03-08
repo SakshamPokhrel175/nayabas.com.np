c3c2cc1e661961bf5a8ff0d6a2f6b55746e036de
Saksham Pokhrel <123399908+SakshamPokhrel175@users.noreply.github.com>
2026-03-08 09:12:16 +0545

    Change API URL to new server address
    Updated the API URL to point to the new server address.

--- nayabas-angular/src/environments/environment.ts
+++ nayabas-angular/src/environments/environment.ts
@@ -1,19 +1,9 @@
 export const environment = {
   production: false,
-
-  apiUrl: ''   //new changee
-  // apiUrl: 'http://localhost:8080'
-  // apiUrl: 'http://192.168.165.62:8080'  // ✅ Use your laptop IP
-
-  
-    // CRITICAL FIX: Changed from 'http://localhost:8080' to your network IP.
-  // The mobile device will now correctly send API requests to your laptop's Spring Boot server.
-    // apiUrl: 'http://192.168.165.113:8080'
-
+  apiUrl: 'http://52.66.212.34:8080'
 };
 
+  //ng serve --host 0.0.0.0
 
-//ng serve --host 0.0.0.0
 
-
-//ng serve --host 0.0.0.0 --proxy-config proxy.conf.json
+  //ng serve --host 0.0.0.0 --proxy-config proxy.conf.json
